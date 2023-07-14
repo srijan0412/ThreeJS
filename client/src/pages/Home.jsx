f8ffed76c42e0ca3401dbc1bd2205ec2d6fe2496
srijan0412 <95132945+srijan0412@users.noreply.github.com>
2023-07-14 22:27:42 +0530

    glitch occured

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
+import state from '../store';
+
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>
-      Home
-    </div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation('left')}>
+          <motion.header>
+            <img 
+              src="./threejs.png" 
+              alt="logo"
+              className='w-8 h-8 object-contain' 
+            />
+          </motion.header>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
